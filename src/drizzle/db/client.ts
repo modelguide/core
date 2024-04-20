7285693059488ba6ed0ee85e7c8ed5b3effa4e47
andrzejewsky <vox3r69@gmail.com>
2024-04-20 21:56:02 +0200

    Connection string

--- src/drizzle/db/client.ts
+++ src/drizzle/db/client.ts
@@ -2,12 +2,19 @@ import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 
-const connectionString = "postgres://postgres:pass123@0.0.0.0:5432/cpq"
+const createConnectionString = () => {
+  const host = process.env.DB_HOST || 'localhost'
+  const user = process.env.DB_USER || ''
+  const password = process.env.DB_PASSWORD || ''
+  const name = process.env.DB_NAME || ''
+
+  return `postgres://${user}:${password}@${host}:5432/${name}`
+}
 
 export type DrizzleDb = PostgresJsDatabase<typeof schema>;
 
 export const createDbClient = <T extends Record<string, postgres.PostgresType> = {}>(options: postgres.Options<T> = {}) => {
-    const connection = postgres<T>(connectionString, options);
+    const connection = postgres<T>(createConnectionString(), options);
 
   return {
     connection, 
